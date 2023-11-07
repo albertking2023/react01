7abe9cb3465d2417cce70573b263e9db373af424
Albert <albertbautistavargas@gmail.com>
2023-11-06 21:45:55 -0500

    Version 5 de sitio React

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import C01componente from './componentes/C01componente';
-import C02contador from './componentes/C02contador';
-import Variables from './componentes/C04variable';
-import C07matrizOperaciones from './componentes/C07matrizOperaciones';
+import AppLista from './protegido/AppLista';
 
+// //style={{background:"violet", width: "350px", padding:"10px"}}
 function App() {
   return (
-    <div className='App'>
-      <h1>Practicas... React</h1>
-      <C01componente />
-      <hr/>
-      <C02contador />
-      <hr/>
-      <Variables xVariable="Lectura a variables" />
-      <hr/>
-      <C07matrizOperaciones />
-      <hr/>
+    <div style={{background:"violet", width: "350px", padding:"10px"}}>
+      <h1>App.js</h1>
+      <AppLista />
     </div>
   );
 }
---
+++ src/protegido/AppForm.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const AppForm = (props) => {
+
+    //style={{background:"orange", padding: "10px"}}
+  return (
+    <div style={{background:"orange", padding: "10px"}}>
+        <form>
+            <button>Cerrar Aplicacion</button>
+
+            <h2>Registrar (AppForm.js)</h2>
+
+            <input name='nonbre' type='text' placeholder='Nombre...' /> <br/>
+            <input name= 'edad' type='text' placeholder='Edad...' /> <br/>
+
+            <select>
+                <option value="">Seleccione género...</option>
+                <option value="Masculino">Masculino</option>
+                <option value="Femenino">Femenino</option>
+            </select> <br/>
+
+            <button>Guardar / Actualizar</button>
+            </form>
+    </div>
+  )
+}
+
+export default AppForm;
